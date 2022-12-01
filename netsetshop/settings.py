338953313f2062efd18c7455109d7f3b2f0a07e5
Benjamin Dusse <fazliddinabduhakimov9@gmail.com>
2022-12-01 08:00:18 -0800

    main cart logic side is completed

--- netsetshop/settings.py
+++ netsetshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'onlineshop.apps.OnlineshopConfig',
-
+    'cart.apps.CartConfig',
 
 ]
 
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
--- onlineshop/models.py
+++ onlineshop/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('product_list_by_category', args=[self.slug])
+        return reverse('onlineshop:product_list_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -38,4 +38,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('product_detail', args=[self.id, self.slug])
+        return reverse('onlineshop:product_detail', args=[self.id, self.slug])
